c4ea44d36e2adaf0ed879538dee44610944b50e1
Israel <israel.vargas.jimenez@hotmail.com>
2019-12-29 01:08:15 +0100

    mejora

--- com/ivj/ejecutora/ClaseEjecutora.java
+++ com/ivj/ejecutora/ClaseEjecutora.java
@@ -1,14 +1,20 @@
 package com.ivj.ejecutora;
 
+import com.ivj.matematicas.CalculaMedia;
+
+import java.util.ArrayList;
+
 import com.ivj.fechas.*; 
 
 public class ClaseEjecutora {
 	public static void main(String[] args) {
      //Crear aqui instancia de la clase a utilizar
 		
-		ClaseFecha fechaHoy = new ClaseFecha();
+		ArrayList<Number> numm = new ArrayList<Number>();
+		numm.add(100);
+		numm.add(75.3);
+		numm.add(40.5);		
 		
-		PosicionDeMemoria posicion = new PosicionDeMemoria(fechaHoy);
-		
+		System.out.println("La media es: "+ String.format("%.2f",CalculaMedia.mediaAritmetica(numm)));		
 	}
 }
--- com/ivj/matematicas/CalculaMedia.java
+++ com/ivj/matematicas/CalculaMedia.java
@@ -1,5 +1,12 @@
 package com.ivj.matematicas;
 
+/**
+ * Clase que calcula la media aritmetica de los numeros pasados como argunmento
+ * en un ArrayList<Number>
+ * @oaram ArrayList<Numer>
+ * @return resultado Double
+ */
+
 import java.util.ArrayList;
 
 public class CalculaMedia {
@@ -17,23 +24,31 @@ public class CalculaMedia {
 	 */
 	public static Number mediaAritmetica(ArrayList<Number> numeros) {
 		//Almacena la suma total de los números
-		Double resultado = 0.0;
-		
-		//Almacena el signo a aplicar al final de la media
-		Double signo = 1.0;
+		Double resultado = 0.0;		
 
 		//Contador de numeros distintos de 0
 		int contador = 0;
-		
 
 		//Se itera por toda la colección sumando los numeros que estén almacenados en el ArrayList<Number>
 		for (Number num : numeros) {	
-			Double numeroDouble = (double) num;	
-			if ( numeroDouble != 0) {				
-				resultado += numeroDouble;
-				contador++;
-			} else {
-				continue;
+			if ( num instanceof Integer) {
+				Integer numeroInteger = (int) num;
+				if ( numeroInteger != 0) {				
+					resultado += numeroInteger;
+					contador++;
+				} else {
+					continue;
+				}
+			}
+
+			if ( num instanceof Double ) {
+				Double numeroDouble = (double) num;	
+				if ( numeroDouble != 0) {				
+					resultado += numeroDouble;
+					contador++;
+				} else {
+					continue;
+				}	
 			}			
 		}
 		resultado = resultado/contador;
