92da280a51ea7d84805c818a3e37b586c1b2a5ae
Israel <israel.vargas.jimenez@hotmail.com>
2020-03-31 08:53:22 +0200

    se crea main y se importan clases utilies necesarias

---
+++ com/ivj/strings/FormarString.java
@@ -0,0 +1,32 @@
+package com.ivj.strings;
+
+public class FormarString {
+
+	public FormarString() {
+		// TODO Auto-generated constructor stub
+	}
+		
+	/**
+	 * Metodo que limita cada frase del array pasado como argumento por referencia
+	 * @param frases
+	 * @param delimitador
+	 */	
+	public static void delimitaFrases (String [] frases, String delimitador) {
+		for ( int i = 0; i < frases.length; i++) {
+			frases[i] = delimitarString(frases[i], delimitador);
+		}
+	}
+	
+	/**
+	 * Metodo que devuelve  la frase pasada como argumento,  marcando  el inicio y el final de la misma
+	 * con un caracter pasado como argumento.
+	 * 
+	 * @param frase String
+	 * @param delimitador String
+	 * @return frase String
+	 *
+	 */
+	public static String  delimitarString ( String frase, String delimitador) {
+		return frase= delimitador.concat(frase).concat(delimitador);		
+	}
+}
