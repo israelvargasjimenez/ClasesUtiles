4f4d8b6702fabf12d03285bff08fcbfb4065beb4
Israel <israel.vargas.jimenez@hotmail.com>
2020-03-31 12:49:37 +0200

    se sigue

--- com/ivj/strings/LecturaEscrituraFicheros.java
+++ com/ivj/strings/LecturaEscrituraFicheros.java
@@ -9,96 +9,93 @@ import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import com.ivj.utiles.LeerDatos;;
 
-
 public class LecturaEscrituraFicheros {
-	//Almacena el path pasado como argumento al constructor para las operaciones finales de salida
-	String path;
+	// Almacena el path pasado como argumento al constructor para las operaciones
+	// finales de salida
+	String pathArchivoOriginal;
 	
-	//Almacena el fichero con la información a tratar
+	//Almacena el path del archivo con las frases tratadas
+	String pathParaSalida;
+
+	// Almacena el fichero con las frases tratadas listas
 	File fichero;
 	
 
-	public LecturaEscrituraFicheros(String path, File fichero) {
-		this.path = path;
-		this.fichero = fichero;
-	}
-	
-	public LecturaEscrituraFicheros(String path) {
-		this.path = path;
-		this.fichero = fichero;
+	/**
+	 * @return the path
+	 */
+	public String getPath() {
+		return pathArchivoOriginal;
 	}
 
+	/**
+	 * @return the fichero
+	 */
+	public File getFichero() {
+		return fichero;
+	}
+
+	public LecturaEscrituraFicheros(String pathFicheroOriginal, String pathParaSalida ) {
+		this.pathArchivoOriginal = pathFicheroOriginal ;	
+		this.pathParaSalida = pathParaSalida;	
+		crearFichero(pathParaSalida);
+	}
 
 	/**
 	 * Metodo que crea un fichero utilizando el path tecleado por el usuario
 	 */
-	public  void crearFichero()  {		
-		File ficheroAux = new File(path);
+	private void crearFichero(String pathParaSalida) {
 		
+		fichero = new File(pathParaSalida);
 		try {
-			ficheroAux.createNewFile();				
+			fichero.createNewFile();
+			
 		} catch (IOException ex) {
 			System.out.println("Se ha producido un error");
-		}catch (SecurityException ex ) {
-			System.out.println("Se ha producido una excepción de seguridad");
-		}		
-	}
-
-	
-	
-	/**
-	 * Metodo que devuelve 1 si ha creado el directorio correctamente devuelve 0 si
-	 * no se ha creado el directorio devuelve -1 si se ha producido una excepción de
-	 * seguiridad. 
-	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por consola.
-	 * @return Devuelve 1 si se ha creado correctamente.Devuelve 0 si no se ha creado correctamente. 
-	 * devuelve -1 si se ha producido una excepción del tipo SecurityException.
-	 */
-	public  void crearDirectorio() {					
-		File ficheroAux = new File(path);
-		try {
-			ficheroAux.mkdir();
-				
 		} catch (SecurityException ex) {
-			System.out.println("Se ha producido un error de seguridad");
-		}		
+			System.out.println("Se ha producido una excepción de seguridad");
+		}
 	}
 
-	
+
+
 	/**
-	 * Metodo que escribe en el fichero final, las lienas 
-	 * absoluta tecleada en consola Devuelve 1 si se ha podido escribir la
-	 * información en el fichero Devuelve 0 si no se ha encontrado el fichero
-	 * Devuelve -1 si se ha producido una excepcion del tipo IOException
-	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por consola.
-	 * @return 1 si se ha podido escribir la información en el fichero	. 
-	 *  -1 si se ha producido una excepcion del tipo IOException
+	 * Metodo que escribe en el fichero final, las lienas absoluta tecleada en
+	 * consola Devuelve 1 si se ha podido escribir la información en el fichero
+	 * Devuelve 0 si no se ha encontrado el fichero Devuelve -1 si se ha producido
+	 * una excepcion del tipo IOException
+	 * 
+	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por
+	 *               consola.
+	 * @return 1 si se ha podido escribir la información en el fichero . -1 si se ha
+	 *         producido una excepcion del tipo IOException
 	 */
-	public  void  escribirEnFichero() {			
-		try {						
-				FileWriter writer = new FileWriter(fichero, true);
-				writer.write(path);
-				writer.flush();
-				writer.close();
-		}catch (IOException e) {			
+	public void escribirEnFichero(String lineaFormada) {
+		try {
+			FileWriter writer = new FileWriter(fichero, true);
+			writer.write(lineaFormada);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
 			System.out.println("Error, el fichero debe ser un directorio");
 			System.out.println("o no se ha podido crear el fichero");
 			System.out.println("o no se ha podido abrir el fichero");
-		}	
+		}
 	}
-	
 
 	/**
-	 * Metodo que leer el fichero pasado como argumento
-	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por consola.
-	 * @param pathArchivoALeer File
+	 * Metodo que se encarga de leer cada linea del fichero y 
+	 * llama a los metodos 
+	 *  
+	 * @param pathFicheroATratar String
 	 */
-	public static void leerFichero(String pathArchivoALeer, boolean debbug) {
+	public void tratarFichero() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(pathArchivoALeer));
+			BufferedReader br = new BufferedReader(new FileReader(pathArchivoOriginal));
 			String lineaLeida;
 			while ((lineaLeida = br.readLine()) != null) {
-				System.out.println(lineaLeida);
+				String lineaFormateada = FormarString.delimitarString(lineaLeida, ",");				
+				escribirEnFichero(lineaFormateada);
 			}
 			br.close();
 		} catch (FileNotFoundException e) {
@@ -107,56 +104,17 @@ public class LecturaEscrituraFicheros {
 			System.out.println("Error al tratar de leer el fichero");
 		}
 	}
-
-	/**
-	 * Metodo que borra el fichero o directorio pasado como argumento Utilizando la
-	 * ubicación del mismo pasada como argumento
-	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por consola.
-	 * @param fichero File
-	 */
-
-	public static void borraFichero( boolean debbug) {
-		String path = devuelvePath();		
-		File fileAux = new File(path);
-		
-		if (fileAux.exists() ) {
-			fileAux.delete();
-		} else {
-			if ( debbug) {
-				mensajeNoEncontrado ();
-			}
-		}
-	}
-
-	/**
-	 * Metodo que borra el fichero o directorio pasado como argumento Utilizando el
-	 * path tecleado por el usuario
-	 * @param debbug Boolean. Si es true se imprimirán los mensajes de error por consola.
-	 */
-	public static void borraFicheroODirectorio(boolean debbug) {
-		String path = devuelvePath();		
-
-		File ficheroAux = new File(path);
-
-		if (ficheroAux.exists() == true) {
-			ficheroAux.delete();
-		} else {
-			if ( debbug) {
-				mensajeNoEncontrado ();
-			}
-		}
-	}
 	
-	public static String devuelvePath () {
-		String path= null;
-		System.out.println();
-		System.out.print("Introduzca el path:");
-		path = LeerDatos.leerString();
-		System.out.println();
-		return path;
-	}
 	
-	private static void mensajeNoEncontrado () {
+	/**
+	 * Metodo que trata cada linea leida del fichero
+	 * 
+	 */
+	public void  darFormatoALinea(String linea, String delimitador) {
+		FormarString.delimitarString(linea, delimitador);
+	}
+
+	private static void mensajeNoEncontrado() {
 		System.out.println("------------------------------------------------");
 		System.out.println("El fichero o directorio no encontrado");
 		System.out.println("------------------------------------------------");
--- com/ivj/strings/main.java
+++ com/ivj/strings/main.java
@@ -1,22 +1,42 @@
 package com.ivj.strings;
 
+import java.util.ArrayList;
+import com.ivj.utiles.LeerDatos;
+
 public class main {
+	// Almacena todas las líneas con las instrucciones insert
+	static ArrayList<String> todasLineasCompletas = new ArrayList<String>();
+
+	// Variable que almacena la linea de turno ya formada completamente
+	static ArrayList<String> lineaCompleta = new ArrayList<String>();
+	
+	
 
 	public main() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
+
+		System.out.println("Teclee el path del fichero a tratar:");
+		String pathFicheroATratar = LeerDatos.leerString();
+		System.out.println("Teclee el path del fichero tratado:");
+		String pathFicheroTratado = LeerDatos.leerString();
 		
-	LecturaEscrituraFicheros.crearFichero(true);
-		
-		
-	
+		LecturaEscrituraFicheros tra = new LecturaEscrituraFicheros(pathFicheroATratar,pathFicheroTratado );		
+		tra.tratarFichero();
 	}
+	
+	
+	/**
+	 * Metodo que escribe una linea tratada completa en ArrayList "todasLineasCompletas"
+	 * @param linea
+	 */
+	public static void guardarLineaCompleta(String linea, String nombre) {
+		//Almacena el nombre de la tabla
+		String nombreTabla=nombre;		
+		String lineaFinal = "INSERT INTO " + nombreTabla+ " values (" + linea+")";
+		todasLineasCompletas.add(lineaFinal);
+	}
+
 }
-	
-
-
-		
-	
-
