b902d874b5cdfaf859060aba992f8c77f0f3b661
Israel <israel.vargas.jimenez@hotmail.com>
2020-04-01 08:12:16 +0200

    se incluye la clase InputStreamReader que admite utf-8

--- com/ivj/strings/LecturaEscrituraFicheros.java
+++ com/ivj/strings/LecturaEscrituraFicheros.java
@@ -3,9 +3,11 @@ package com.ivj.strings;
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.io.FileReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import com.ivj.utiles.LeerDatos;;
@@ -94,7 +96,7 @@ public class LecturaEscrituraFicheros {
 	public void tratarFichero() {
 		Charset set = Charset.defaultCharset();
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(pathArchivoOriginal));
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream (pathArchivoOriginal),"UTF-8"));
 			String lineaLeida;
 			while ((lineaLeida = br.readLine()) != null) {
 				//String lineaFormateada = FormarString.delimitarString(lineaLeida, ",",false, true);		
