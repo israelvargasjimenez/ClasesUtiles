818ec246021a2c6529e7f12b160b61ce36e8b831
Israel <israel.vargas.jimenez@hotmail.com>
2020-04-05 10:58:37 +0200

    SE MODIFICA

--- com/ivj/creaTablaSQLSERVER/FormarString.java
+++ com/ivj/creaTablaSQLSERVER/FormarString.java
@@ -6,8 +6,6 @@ public class FormarString {
 		// TODO Auto-generated constructor stub
 	}
 
-
-
 	/**
 	 * Metodo que devuelve la frase pasada como argumento, marcando el inicio y el
 	 * final de la misma con un caracter pasado como argumento.
@@ -16,22 +14,27 @@ public class FormarString {
 	 * @param delimitador String
 	 * @return frase String
 	 *
-	 */	
-	public static String delimitarString(String frase, boolean ultima) {
-		frase = "".concat(frase.strip());
-		if (ultima) {
-			return frase = "'".concat(frase).concat("'");
+	 */
+	public static String delimitarString(String frase, boolean ultima, boolean numero) {
+		//frase = "".concat(frase.strip());
+		if (numero) {
+			return frase = frase.concat(",");
+		} else {
+			if (ultima) {
+				return frase = "'".concat(frase).concat("'");
+			}
 		}
 		return frase = "'".concat(frase).concat("'").concat(",");
 	}
-	
+
 	/**
-	 * Metodo que sustituye el caracter oldCaracter por el caracter newCaracter
-	 * en la frase pasada como argumento
+	 * Metodo que sustituye el caracter oldCaracter por el caracter newCaracter en
+	 * la frase pasada como argumento
+	 * 
 	 * @param frase String
 	 * @return frase String
 	 */
-	public static String sustituyeCaracteres(String frase, char oldCaracter, char newCaracter) {		
-		return frase.replace(oldCaracter,newCaracter);		
+	public static String sustituyeCaracteres(String frase, char oldCaracter, char newCaracter) {
+		return frase.replace(oldCaracter, newCaracter);
 	}
 }
