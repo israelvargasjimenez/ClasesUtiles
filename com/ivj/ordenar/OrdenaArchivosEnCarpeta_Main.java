e3f03ac11d2264b7fa4f4ed02711fa1b656c69be
cuarto <cuarto@cuarto-PC>
2020-06-16 19:08:10 +0200

    se sigue

--- com/ivj/ordenar/OrdenaArchivosEnCarpeta_Main.java
+++ com/ivj/ordenar/OrdenaArchivosEnCarpeta_Main.java
@@ -1,6 +1,8 @@
 package com.ivj.ordenar;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.FileSystem;
 import java.util.Arrays;
 
@@ -17,22 +19,27 @@ import com.ivj.utiles.LeerDatos;
 public class OrdenaArchivosEnCarpeta_Main {
 
 	public static void main(String[] args) {
-		//Pide y almacena el path del directorio a ordenar
-		String pathCarpetaDesordenada = "A:\\CURSOS ISRAEL PARA REALIZAR\\Curso java pildoras"; //devuelvePath();
-		
+		// Pide y almacena el path del directorio a ordenar
+		String pathCarpetaDesordenada = "A:\\CURSOS ISRAEL PARA REALIZAR\\Curso java pildoras"; // devuelvePath();
+
 		// Path del fichero antes de ordenarse
 		String pathDeFichero;
 
 		// Listado de los path de todos los archivos dentro del directorio a ordenar
 		File archivo = new File(pathCarpetaDesordenada);
 		File[] todosArchivos = archivo.listFiles();
-		
-		for (int i = 0 ; i< todosArchivos.length; i++) {
+
+		for (int i = 0; i < todosArchivos.length; i++) {
 			String pathString = todosArchivos[i].getName();
 			System.out.println(pathString);
+			if(pathString.contains("Páginas")) {
+				File file = new File(pathString);
+				boolean eliminado = file.delete();
+				System.out.println(eliminado);
+			}
 		}
-		
 	}
+	
 
 	/**
 	 * Metodo que pide y devuelve el path del directorio a ordenar en carpetas
