e617f088180e0c658556fd90a3206621d6b6b8a9
israelvargasjimenez <58093163+israelvargasjimenez@users.noreply.github.com>
2020-08-30 15:24:43 +0200

    Se repara error, el número 2 se consideraba no primo

--- com/ivj/numeros/Numeros.java
+++ com/ivj/numeros/Numeros.java
@@ -11,20 +11,12 @@ public class Numeros {
 	 * @return true: Si el número es primo
 	 */
 	public static boolean isPrimo(int numero) throws Exception {
-
-		int numeroAnsoluto = Math.abs(numero);
-
-		if (numeroAnsoluto == 0) {
+		if (Math.abs(numero) == 0) {
 			throw new Exception();
-		}
+		}		
 
-		// Si el número es 2 o -2, se devuelve false
-		if (numeroAnsoluto == 2) {
-			return false;
-		}
-
-		for (int i = 2; i < numeroAnsoluto; i++) {
-			if (numeroAnsoluto % i == 0) {
+		for (int i = 2; i < Math.abs(numero); i++) {
+			if (Math.abs(numero) % i == 0) {
 				return false;
 			}
 		}
